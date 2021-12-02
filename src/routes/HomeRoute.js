55f1e87abe8f7daf3974cb1753eee25aae8f4a23
mjesusoj <m.jesusojedasalvador@gmail.com>
2021-12-02 16:12:07 +0100

    Remove MoneyRoute and includes in HomeRoute

--- src/routes/HomeRoute.js
+++ src/routes/HomeRoute.js
@@ -2,26 +2,36 @@ import React from 'react';
 
 import { Switch, Route, Redirect } from 'react-router';
 
-import { Header } from '../components/ui/Header';
-import { CookieVerify } from '../components/Cookies/CookieVerify';
-
 import { AboutScreen } from '../components/AboutScreen/AboutScreen';
-import { LinksScreen } from '../components/LinksScreen/LinksScreen';
+import { AttaPollScreen } from '../components/MoneyWin/Surveys/AttaPoll/AttaPollScreen';
 import { BanksScreen } from '../components/BanksScreen/BanksScreen';
 import { BlogScreen } from '../components/BlogScreen/BlogScreen';
-import { HomeScreen } from '../components/HomeScreen/HomeScreen';
-
+import { CashbackScreen } from '../components/MoneyEarn/Cashback/CashbackScreen';
+import { CookieVerify } from '../components/Cookies/CookieVerify';
 import { CryptosScreen } from '../components/MoneyWin/Cryptos/CryptosScreen';
+import { GameeScreen } from '../components/MoneyWin/Playing/Gamee/GameeScreen';
+import { Header } from '../components/ui/Header';
+import { HomeScreen } from '../components/HomeScreen/HomeScreen';
+import { HoneyGainScreen } from '../components/MoneyWin/Nothing/HoneyGain/HoneyGainScreen';
+import { IgraalScreen } from '../components/MoneyEarn/Cashback/Igraal/IgraalScreen';
+import { IpsosIsayScreen } from '../components/MoneyWin/Surveys/Ipsos-isay/IpsosIsayScreen';
+import { LetyShopsScreen } from '../components/MoneyEarn/Cashback/Letyshops/LetyShopsScreen';
+import { LinksScreen } from '../components/LinksScreen/LinksScreen';
+import { MarketAgentScreen } from '../components/MoneyWin/Surveys/Marketagent/MarketAgentScreen';
 import { NothingScreen } from '../components/MoneyWin/Nothing/NothingScreen';
 import { PlayingScreen } from '../components/MoneyWin/Playing/PlayingScreen';
+import { PollPayScreen } from '../components/MoneyWin/Surveys/PollPay/PollPayScreen';
+import { PrizeRebelScreen } from '../components/MoneyWin/Surveys/PrizeRebel/PrizeRebelScreen';
+import { PromosScreen } from '../components/MoneyEarn/TicketsScreen/Promos/PromosScreen';
+import { RobinScreen } from '../components/MoneyEarn/Cashback/RobinRover/RobinScreen';
+import { SmartmeScreen } from '../components/MoneyWin/Surveys/SmartmeApp/SmartmeScreen';
 import { SurveysScreen } from '../components/MoneyWin/Surveys/SurveysScreen';
-
-import { CashbackScreen } from '../components/MoneyEarn/Cashback/CashbackScreen';
 import { TicketsScreen } from '../components/MoneyEarn/TicketsScreen/TicketsScreen';
-
-import { MoneyRoute } from './MoneyRoute';
+import { TiendeoScreen } from '../components/MoneyEarn/TicketsScreen/Tiendeo/TiendeoScreen';
 import { VerseScreen } from '../components/BanksScreen/Verse/VerseScreen';
 import { VividScreen } from '../components/BanksScreen/VividMoney/VividScreen';
+import { WidiloScreen } from '../components/MoneyEarn/Cashback/Widilo/WidiloScreen';
+import { YouGovScreen } from '../components/MoneyWin/Surveys/YouGov/YouGovScreen';
 
 export const HomeRoute = () => {
     return (
@@ -51,8 +61,30 @@ export const HomeRoute = () => {
                 <Route exact path="/verse-app" component={ VerseScreen } />
                 <Route exact path="/vivid-money" component={ VividScreen } />
 
-                {/* Routes for All Money */ }
-                <MoneyRoute />
+                { /* Routes Cashback */ }
+                <Route exact path="/igraal" component={ IgraalScreen } />
+                <Route exact path="/letyshops" component={ LetyShopsScreen } />
+                <Route exact path="/robin-rover" component={ RobinScreen } />
+                <Route exact path="/widilo" component={ WidiloScreen } />
+
+                { /* Routes Cashback Tickets */ }
+                <Route exact path="/promos-app" component={ PromosScreen } />
+                <Route exact path="/tiendeo" component={ TiendeoScreen } />
+
+                { /* Routes Nothing - HoneyGain, IPRoyal Panws */}
+                <Route exact path="/honeygain" component={ HoneyGainScreen } />
+
+                { /* Routes for Surveys */ }
+                <Route exact path="/attapoll" component={ AttaPollScreen } />
+                <Route exact path="/ipsos-isay" component={ IpsosIsayScreen } />
+                <Route exact path="/marketagent" component={ MarketAgentScreen } />
+                <Route exact path="/pollpay" component={ PollPayScreen } />
+                <Route exact path="/prize-rebel" component={ PrizeRebelScreen } />
+                <Route exact path="/smartme-app" component={ SmartmeScreen } />
+                <Route exact path="/yougov" component={ YouGovScreen } />
+
+                { /* Routes Games */ }
+                <Route exact path="/gamee" component={ GameeScreen } />
 
                 {/* Route 404 */}
                 <Route path="*" component={ HomeScreen } />
@@ -63,8 +95,6 @@ export const HomeRoute = () => {
             <div className="p-3 mt-2 text-dark row footer">
                 <CookieVerify />
             </div>
-
-            {/* // Include <Footer and param/> */}
         </>
     )
 }
