def847a3c7331ec67a311b357cd2834de03e95a8
mjesusoj <m.jesusojedasalvador@gmail.com>
2022-01-24 19:40:32 +0100

    Indent in posts and more :art:

--- src/data/posts.js
+++ src/data/posts.js
@@ -1,94 +1,88 @@
-export const posts = [
-    {
-        id: 0,
-        title: "🥇 El mejor banco online con cashback | Vivid Money 💜",
-        description: `Descubre el mejor banco online y TODAS las SUPER ventajas 💜 que tiene 🥳
-            Únete y obtén tarjeta virtual y Metálica Visa Gratis, 3 Meses de Cashback,
-            1 Mes de SUPER Cashback y 5€ por registro 💸 (AHORA HAY NUEVOS CAMBIOS 🔴)`,
-        image: "../img/craftianosmoney.webp",
-        link: "/vivid-money/",
-        date: "Publicado el 09/03/2021"
-    },
-    {
-        id: 1,
-        title: '👛 Plataformas de cashback | Post en @craftianosmoney',
-        description: `En este post podeís ver las plataformas de cashback que recomiendo y que utilizo, 
-            dicha publicación está en mi Instagram (@craftianosmoney) | 
-            Donde subo mucho contenido relacionado con ganar dinero gratis, ya sea haciendo encuestas,
-            haciendo compras online, promociones como Verse (5€ GRATIS con el código: BJNQ8R 🥇💎)
-            y muchas más 🤯👏 
-            También puedes ir a la sección dedicada en esta página 👉
-            <a href="../compras-cashback/">pulsa aquí 👋</a>`,
-        image: '../img/otros/post_cashback.png',
-        link: 'https://bit.ly/cashbackPostCR',
-        date: 'Publicado el 23/01/2021'
-    },
-    {
-        id: 2, 
-        title: '💙 Verse: Recibe 5€ al UNIRTE y EMPIEZA A HACER PAGOS RAPIDOS 🤯',
-        description: `Únete a Verse, haz pagos con amigos, 
-            GANA DINERO PARTICIPANDO EN TRIVIALS, entre otras MUCHAS COSAS 🧐✅ | 
-            GANA 5€ GRATIS AL UNIRTE A VERSE CON MI CODE: BJNQ8R 💙`,
-        image: '../img/verse/verse.png',
-        link: '/verse-app/',
-        date: 'Publicado el 02/12/2020'
-    },
-    {
-        id: 3,  
-        title: 'Robin Rover: Gana DINERO por ceder tus datos de compra',
-        description: `Con Robin Rover puedes recibir dinero por ceder tus datos de compra, 
+export const posts = [{
+    id: 0,
+    title: "🥇 El mejor banco online con cashback | Vivid Money 💜",
+    description: `Descubre el mejor banco online y TODAS las SUPER ventajas 💜 que tiene 🥳
+        Únete y obtén tarjeta virtual y Metálica Visa Gratis, 3 Meses de Cashback,
+        1 Mes de SUPER Cashback y 5€ por registro 💸 (AHORA HAY NUEVOS CAMBIOS 🔴)`,
+    image: "../img/craftianosmoney.webp",
+    link: "/vivid-money/",
+    date: "Publicado el 09/03/2021"
+},
+{
+    id: 1,
+    title: '👛 Plataformas de cashback | Post en @craftianosmoney',
+    description: `En este post podeís ver las plataformas de cashback que recomiendo y que utilizo, 
+        dicha publicación está en mi Instagram (@craftianosmoney) | 
+        Donde subo mucho contenido relacionado con ganar dinero gratis, ya sea haciendo encuestas,
+        haciendo compras online, promociones como Verse (5€ GRATIS con el código: BJNQ8R 🥇💎)
+        y muchas más 🤯👏 `,
+    image: '../img/otros/post_cashback.png',
+    link: 'https://bit.ly/cashbackPostCR',
+    date: 'Publicado el 23/01/2021'
+},
+{
+    id: 2, 
+    title: '💙 Verse: Recibe 5€ al UNIRTE y EMPIEZA A HACER PAGOS RAPIDOS 🤯',
+    description: `Únete a Verse, haz pagos con amigos, 
+        GANA DINERO PARTICIPANDO EN TRIVIALS, entre otras MUCHAS COSAS 🧐✅ | 
+        GANA 5€ GRATIS AL UNIRTE A VERSE CON MI CODE: BJNQ8R 💙`,
+    image: '../img/verse/verse.png',
+    link: '/verse-app/',
+    date: 'Publicado el 02/12/2020'
+},
+{
+    id: 3,  
+    title: 'Robin Rover: Gana DINERO por ceder tus datos de compra',
+    description: `Con Robin Rover puedes recibir dinero por ceder tus datos de compra, 
         ya que el consumidor debería de recibir una compensación por venderse sus datos. <br /><br />
         Entra aquí 👋 y empieza a recibir el dinero que deberías de obtener.`,
-        image: '../img/robin-rover/image.png',
-        link: '/robin-rover/',
-        date: 'Publicado el 04/10/2020'
-    },
-    {
-        id: 4,
-        title: 'Promos App: Recibe dinero por comprar en supermercados',
-        description: `Promos es una app con la que podemos ganar dinero por hacer la compra en nuestros supermercados favoritos. 
+    image: '../img/robin-rover/image.png',
+    link: '/robin-rover/',
+    date: 'Publicado el 04/10/2020'
+},
+{
+    id: 4,
+    title: 'Promos App: Recibe dinero por comprar en supermercados',
+    description: `Promos es una app con la que podemos ganar dinero por hacer la compra en nuestros supermercados favoritos. 
         ¿A qué esperas para recibir dinero por hacer la compra?`,
-        image: '../img/promos/promos.png',
-        link: '/promos-app/',
-        date: 'Publicado el 04/10/2020'
-    },
-    {
-        id: 5,   
-        title: 'Widilo: Compras en tiendas online y recibe cashback | 5€ gratis',
-        description: `Widilo es otra de las mejores plataformas de cashback, con la que obtendremos 5€ GRATIS
+    image: '../img/promos/promos.png',
+    link: '/promos-app/',
+    date: 'Publicado el 04/10/2020'
+},
+{
+    id: 5,   
+    title: 'Widilo: Compras en tiendas online y recibe cashback | 5€ gratis',
+    description: `Widilo es otra de las mejores plataformas de cashback, con la que obtendremos 5€ GRATIS
         al realizar nuestra primera compra, sin mínimo.`,
-        image: '../img/widilo/widilo.webp',
-        link: '/widilo/',
-        date: 'Publicado el 20/09/2020'
-    },
-    {
-        id: 6,  
-        title: 'SmartmeApp - Encuestas cortas y mucho más 👏',
-        description: `Smartme App es una aplicación multiplataforma, con la que podremos realizar
-        encuestas cortas, tenerla abierta en segundo plano, invitar a amigos y recibir cashback con
-        el club smartme.`,
-        image: '../img/smartme-app/smartmeapp.png',
-        link: '/smartme-app/',
-        date: 'Publicado el 09/09/2020'
-    },
-    {
-        id: 7,  
-        title: 'LetyShops - Plataforma de cashback (De las mejores) 🤯',
-        description: `Letyshops es una plataforma de cashback, 
-        con la que nos devolverá dinero por realizar compras en más de 2.000 tiendas online, si se registra hoy,
-        podrá ganar 5€ gratis al hacer su primera compra de 20€.`,
-        image: '../img/letyshops/letyshops.webp',
-        link: '/letyshops/',
-        date: 'Publicado el 09/09/2020'
-    },
-    {
-        id: 8, 
-        title: 'YouGov - Encuestas cortas y canjeo en cupones de Amazon',
-        description: `YouGov es de la mejor plataforma de encuestas online, está disponible para todo el mundo.
+    image: '../img/widilo/widilo.webp',
+    link: '/widilo/',
+    date: 'Publicado el 20/09/2020'
+},
+{
+    id: 6,  
+    title: 'SmartmeApp - Encuestas cortas y mucho más 👏',
+    description: `Smartme App es una aplicación multiplataforma, con la que podremos realizar
+        encuestas cortas, tenerla abierta en segundo plano, invitar a amigos y recibir cashback con el club smartme.`,
+    image: '../img/smartme-app/smartmeapp.png',
+    link: '/smartme-app/',
+    date: 'Publicado el 09/09/2020'
+},
+{
+    id: 7,  
+    title: 'LetyShops - Plataforma de cashback (De las mejores) 🤯',
+    description: `Letyshops es una plataforma de cashback, con la que nos devolverá dinero por realizar compras en más de 2.000 tiendas online, 
+        si se registra hoy, podrá ganar 5€ gratis al hacer su primera compra de 20€.`,
+    image: '../img/letyshops/letyshops.webp',
+    link: '/letyshops/',
+    date: 'Publicado el 09/09/2020'
+},
+{
+    id: 8, 
+    title: 'YouGov - Encuestas cortas y canjeo en cupones de Amazon',
+    description: `YouGov es de la mejor plataforma de encuestas online, está disponible para todo el mundo.
         Con compartir nuestra opinión en diferentes estudios, podemos ganar 25€ en cupones de Amazon y 50€.
         Mira más información sobre YouGov y regístrate para empezar a ganar premios. 👈`,
-        image: '../img/yougov/yougov.png',
-        link: '/yougov/',
-        date: 'Publicado el 07/09/2020'
-    }
-];
+    image: '../img/yougov/yougov.png',
+    link: '/yougov/',
+    date: 'Publicado el 07/09/2020'
+}];
