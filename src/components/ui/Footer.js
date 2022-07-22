a7a22466f5694e7c571505a112a7050ba8019542
mjesusoj <m.jesusojedasalvador@gmail.com>
2022-07-22 11:35:21 +0200

    Remove Google Analytics and now use Plausible #6 :lock::rocket:

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -2,13 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export const Footer = () => {
+    const btnGoUp = () => {
+        window.scrollTo(0, 0)
+    }
+
     return (
         <>
             <div className="col-12 footer mt-2">
                 <p className='text-justify text-center'>
                     Todos los derechos reservados © Craftianos 2022
-                    <img loading="lazy" src="../img/icons/arrow.svg" className="go-up figure-img img-fluid rounded mt-2" alt="arrow" />
                 </p>
+                <button onClick={ btnGoUp } className='go-up'>
+                    <img loading="lazy" src="../img/icons/arrow.svg" className="figure-img img-fluid rounded mt-2" alt="arrow" />
+                </button>
             </div>
             <div className="col-12 footer enlaces">
                 <ul className="nav nav-pills justify-content-center enlacesfooter">
