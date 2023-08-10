72c45290687f98f307809f08b1c8e3bd308414e2
mjesusoj <mjesusoj@proton.me>
2023-08-10 20:15:22 +0200

    Add aptabase analytics, privacy first and remove beampipe :rocket::closed_lock_with_key:

--- src/CraftianosPrivacy.jsx
+++ src/CraftianosPrivacy.jsx
@@ -3,6 +3,14 @@ import { Provider } from 'react-redux';
 import { store } from './store/store';
 import { AppRouter } from './routes/AppRouter';
 
+import { init } from "@aptabase/web";
+init("A-EU-3117997246"); // 👈 this is where you enter your App Key
+
+import { trackEvent } from "@aptabase/web";
+
+trackEvent("app_started"); // An event with no properties
+trackEvent("page_view", { name: "Settings", path: "/settings" });
+
 export const CraftianosPrivacy = () => {  
     return (
         <Provider store={ store }>
