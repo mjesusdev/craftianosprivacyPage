74935ad92e494e03c10ffbc9adb5f01a8b3314bf
mjesusoj <mjesusoj@proton.me>
2023-08-10 20:24:17 +0200

    Change location to trackevent Aptabase :rocket:

--- src/CraftianosPrivacy.jsx
+++ src/CraftianosPrivacy.jsx
@@ -6,11 +6,6 @@ import { AppRouter } from './routes/AppRouter';
 import { init } from "@aptabase/web";
 init("A-EU-3117997246"); // 👈 this is where you enter your App Key
 
-import { trackEvent } from "@aptabase/web";
-
-trackEvent("app_started"); // An event with no properties
-trackEvent("page_view", { name: "Settings", path: "/settings" });
-
 export const CraftianosPrivacy = () => {  
     return (
         <Provider store={ store }>
--- src/components/HomeScreen/index.js
+++ src/components/HomeScreen/index.js
@@ -1,1 +1,5 @@
+import { trackEvent } from "@aptabase/web";
 export * from './HomeScreen'
+
+trackEvent("app_started"); // An event with no properties
+trackEvent("page_view", { name: "Home Page", path: "/home" });
